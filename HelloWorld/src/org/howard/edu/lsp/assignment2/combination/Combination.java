87ba6a205b5b61585f4e5b4df7c8bde123bf0ce0
TolulopeAkinlabi <tolulope.akinlabi@bison.howard.edu>
2021-02-01 21:24:55 -0500

    Updated Homework2

--- HelloWorld/src/org/howard/edu/lsp/assignment2/combination/Combination.java
+++ HelloWorld/src/org/howard/edu/lsp/assignment2/combination/Combination.java
@@ -1,22 +1,49 @@
 package org.howard.edu.lsp.assignment2.combination;
+import java.io.*;
+import java.util.*;
 
 /**
  * 
  * @author Tolulope Akinlabi
+ * @version 1.0
+ * @since 02-01-2021
  *  
  */
 
 public class Combination {
 	
-	//instantiates the arrays of integers and its target sum
+	/**
+	 * 
+	 * @param args
+	 * @author Tolulope Akinlabi
+	 */
 	
-	public void implementation() {
+	public static void newArray(String[] args) { 
 		
-		int numArrayOneTargetSum = 12;
-		int numArrayTwoTargetSum = 70;
-		
+		//initializing arrays and target value
 		int[] numArrayOne = {2,4,6,8,10,12};
 		int[] numArrayTwo = {10,20,30,40,50,60,70};
-
+		
+		int targetOfArrayOne = 12;
+		int targetOfArrayTwo = 70;
+		int[] checker;
+		int[] check;
+		
+		//Check that sum in Array One does not become negative
+		for (int i = 0; i < numArrayOne.size(); i++) {
+			
+			if ((targetOfArrayOne - numArrayOne.get(i)) >= 0) {
+				checker.add(numArrayOne.get(i));
+			}
+		}
+		
+		//Check that sum in Array Two does not become negative
+		
+		for (int a = 0; a < numArrayTwo.size(); a++) {
+			
+			if ((targetOfArrayTwo - numArrayTwo.get(a)) >= 0) {
+				check.add(numArrayOne.get(a));
+			}
+		}
 	}
 }
--- HelloWorld/src/org/howard/edu/lsp/assignment2/test/Test.java
+++ HelloWorld/src/org/howard/edu/lsp/assignment2/test/Test.java
@@ -1,25 +1,62 @@
 package org.howard.edu.lsp.assignment2.test;
 import org.howard.edu.lsp.assignment2.combination.*;
 
+/**
+ * 
+ * @author Tolulope Akinlabi
+ * @version 1.0
+ * @since 02-01-2021
+ *  
+ */
+
 public class Test extends Combination {
+	
+	/**
+	 * 
+	 * @param args
+	 * @author Tolulope Akinlabi
+	 */
+	
 	
 	//DriverCode - Invokes and test implementation
 	
 	public static void main (String[] args){
 		
-		//Print result for first array
-		System.out.println(numArrayOne[0] + numArrayOne[1] + numArrayOne[2]);
-		System.out.println(numArrayOne[0] + numArrayOne[4]);
-		System.out.println(numArrayOne[1] + numArrayOne[3]);
-		System.out.println(numArrayOne[2] + numArrayOne[2]);
-		System.out.println(numArrayOne[5]);
+		//if array is empty, return a note
+		if(numArrayOne.size() == 0) {
+			System.out.println("Empty Array");
+			return;
+		}
 		
-		//Print result for second array
-		System.out.println(numArrayTwo[0] + numArrayTwo[1] + numArrayTwo[3]);
-		System.out.println(numArrayTwo[0] + numArrayTwo[5]);
-		System.out.println(numArrayTwo[1] + numArrayTwo[4]);
-		System.out.println(numArrayTwo[2] + numArrayTwo[3]);
-		System.out.println(numArrayTwo[6]);
-	}
-	
+		if(numArrayTwo.size() == 0) {
+			System.out.println("Empty Array");
+			return;
+		}
+		
+
+		//print all the possible sum combinations of arrayOne
+		
+		for (int i=0; i < numArrayOne.size(); i++) {
+			
+			System.out.println("(");
+			for (int j = 0; j < numArrayOne.size(); j++) {
+				System.out.println(numArrayOne.get(i).get(j) + " ");
+			}
+			
+			System.out.println(")");
+		}
+		
+		
+		// print all possible combination of arrayTwo
+		
+		for (int a = 0; a < numArrayTwo.size(); a++) {
+			
+			System.out.println("(");
+			for (int b = 0; b < numArrayTwo.size(); b++) {
+				System.out.println(numArrayTwo.get(a).get(b) + " ");
+			}
+			
+			System.out.println(")");
+		}
+	}	
 }
